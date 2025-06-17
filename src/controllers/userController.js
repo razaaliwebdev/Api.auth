c05632a40e6ca3b520f0a8410d625d8aadd34a08
Raza Ali <razaalli.webdev@gmail.com>
2025-06-17 13:30:02 +0500

    fix register controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -24,14 +24,8 @@ export const register = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const user = await User.create(
-            {
-                name,
-                email,
-                password: hashedPassword,
-                avatar: req.file.path    // Store the image path
-            }
-        );
+        const user = await User.create({ name, email, password: hashedPassword });
+
 
         const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
